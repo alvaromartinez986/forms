b2ba1d884929ca2f3801f582eff5220dc0c83f7d
Alvaro <martinez.alvaro@correounivalle.edu.co>
2017-08-12 00:47:52 -0500

    Add validation messages to name

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -9,13 +9,31 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class DataComponent{
 
   form:FormGroup;
+  user:Object = {
+    fullname : {
+      name : "Alvaro",
+      lastname : "Martinez"
+    },
+    email : "msae986@gmail.com"
+  }
 
   constructor() {
     this.form = new FormGroup({
-      'name': new FormControl('Alvaro'),
-      'lastname': new FormControl(),
-      'email': new FormControl(),
 
+      'fullname': new FormGroup({
+        'name': new FormControl('', [
+                                      Validators.required,
+                                      Validators.minLength(3)
+                                    ]
+                                ),
+        'lastname': new FormControl('',  Validators.required)
+
+      }),
+      'email': new FormControl('', [
+                                      Validators.required,
+                                      Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')
+                                   ]
+                              )
     });
   }
 
