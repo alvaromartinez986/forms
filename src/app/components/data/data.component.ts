a2ac5722fa670060540cf62a41ca6f1904547772
Maleja <martinez.alvaro@correounivalle.edu.co>
2017-08-13 00:27:59 -0500

    Add asycronous vaidation, changes values

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,21 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
   styles: []
 })
-export class DataComponent{
+export class DataComponent {
 
-  form:FormGroup;
-  user:Object = {
-    fullname : {
-      name : "Alvaro",
-      lastname : "Martinez"
+  form: FormGroup;
+  user: Object = {
+    fullname: {
+      name: "Alvaro",
+      lastname: "Martinez"
     },
-    email : "msae986@gmail.com",
-    hobbies : ["Correr", "Comer"]
+    email: "msae986@gmail.com",
+    hobbies: ["Correr", "Comer"]
   }
 
   constructor() {
@@ -23,43 +24,103 @@ export class DataComponent{
 
       'fullname': new FormGroup({
         'name': new FormControl('', [
-                                      Validators.required,
-                                      Validators.minLength(3)
-                                    ]
-                                ),
-        'lastname': new FormControl('',  Validators.required)
+          Validators.required,
+          Validators.minLength(3)
+        ]
+        ),
+        'lastname': new FormControl('', [
+                                          Validators.required,
+                                          this.noHerrera
+                                        ])
 
       }),
       'email': new FormControl('', [
-                                      Validators.required,
-                                      Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')
-                                   ]
-                              ),
+        Validators.required,
+        Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')
+      ]
+      ),
       'hobbies': new FormArray([
-          new FormControl('Correr', Validators.required)
-      ])
+        new FormControl('Correr', Validators.required)
+      ]),
+      'username': new FormControl('', Validators.required, this.existUser),
+      'passwordOne': new FormControl('', Validators.required),
+      'passwordTwo': new FormControl()
     });
 
     //this.form.setValue(this.user);
+
+    this.form.controls['passwordTwo'].setValidators([
+      Validators.required,
+      this.noEqual.bind(this)
+    ]);
+
+
+    this.form.valueChanges
+        .subscribe( data=>{
+
+        });
   }
 
-  addHobbie(){
+  addHobbie() {
     (<FormArray>this.form.controls['hobbies']).push(
       new FormControl('Dormir', Validators.required)
     );
   }
 
+noHerrera(control:FormControl): { [s:string]:boolean }{
 
-  save() {
-    console.log(this.form.value);
-    this.form.reset({
-      fullname:{
-        name:"",
-        lastname:""
-      },
-      email:"",
-      hobbies : [""]
-    });
-  }
+
+    if( control.value==="herrera" ){
+      return{
+        noherrera:true
+      }
+    }
+
+  return null;
+}
+
+existUser(control:FormControl):Promise<any>|Observable<any>{
+
+  let promise = new Promise(
+    (resolve, reject)=>{
+      setTimeout(()=>{
+        if(control.value==="strider"){
+          resolve({existe:true})
+        }else{
+          resolve(null)
+        }
+      }, 3000)
+    }
+  )
+
+  return promise;
+
+}
+
+
+noEqual(control:FormControl): { [s:string]:boolean }{
+
+
+    if( control.value!==this.form.controls['passwordOne'].value ){
+      return{
+        noequal:true
+      }
+    }
+
+  return null;
+}
+
+
+save() {
+  console.log(this.form.value);
+  // this.form.reset({
+  //   fullname: {
+  //     name: "",
+  //     lastname: ""
+  //   },
+  //   email: "",
+  //   hobbies: [""]
+  // });
+}
 
 }
